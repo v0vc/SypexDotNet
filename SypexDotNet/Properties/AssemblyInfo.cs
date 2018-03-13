bff514f341fa79c4912da4613d16a5eb7997c9f8
vkoshelev <vkoshelev@luxoft.com>
2018-03-13 18:03:09 +0300

    Remove old nuget packages

--- SypexDotNet/Properties/AssemblyInfo.cs
+++ SypexDotNet/Properties/AssemblyInfo.cs
@@ -8,7 +8,7 @@ using System.Runtime.InteropServices;
 [assembly: AssemblyTitle("SypexDotNet")]
 [assembly: AssemblyDescription("")]
 [assembly: AssemblyConfiguration("")]
-[assembly: AssemblyCompany("")]
+[assembly: AssemblyCompany("v0v")]
 [assembly: AssemblyProduct("SypexDotNet")]
 [assembly: AssemblyCopyright("Copyright ©  2018")]
 [assembly: AssemblyTrademark("")]
